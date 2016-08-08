b6c0cf6b12f952f35cb0e899c3143d194db9801c
Ryan Chenkie <chenkie@users.noreply.github.com>
2016-08-07 20:55:29 -0400

    Update app.js

--- examples/widget-redirect/app.js
+++ examples/widget-redirect/app.js
@@ -16,8 +16,8 @@ app.config( function myAppConfig ( $routeProvider, authProvider, $httpProvider, 
 
 
   authProvider.init({
-    domain: 'AUTH0_DOMAIN',
-    clientID: 'AUTH0_CLIENT_ID',
+    domain: AUTH0_DOMAIN,
+    clientID: AUTH0_CLIENT_ID,
     loginUrl: '/login'
   });
 
